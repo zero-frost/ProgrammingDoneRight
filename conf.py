b3a4b157c1618d493bcc5c87a469cdddcd872a40
Tim Winters <twinters@wpi.edu>
2017-03-12 09:20:37 -0400

    Use latex for equations. Add REAL NUMBER macro

--- conf.py
+++ conf.py
@@ -32,8 +32,11 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo',
-    'sphinx.ext.mathjax', 'sphinx_tabs.tabs']
+    'sphinx.ext.imgmath', 'sphinx_tabs.tabs']
 
+imgmath_latex_preamble = '''
+    \\newcommand{\\R}{\\mathbb{R}}
+'''
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
 
@@ -65,7 +68,7 @@ release = '0.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
